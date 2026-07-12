85a5634d8eb48e359682f970f79a0955eaf04ee8
univers99 <axelprimael@gmail.com>
2026-07-12 19:48:01 +0200

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,11 +6,19 @@ use Illuminate\Support\ServiceProvider;
 
 use Illuminate\Pagination\Paginator;
 
-
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
+   
+public function boot(): void
+{
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
+     Paginator::useBootstrap();
+}
+/**
      * Register any application services.
      */
     public function register(): void
@@ -28,8 +36,5 @@ protected $listen = [
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-    Paginator::useBootstrap();
-    }
+
 }
